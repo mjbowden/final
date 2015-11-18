5b8ac8bceb48693f5f5f996322cbeb09286e2ab0
Matt <mjbowden@quinnipiac.edu>
2015-11-18 20:26:53 +0000

    Add review list view

--- core/views.py
+++ core/views.py
@@ -3,6 +3,7 @@ from django.views.generic import TemplateView
 from django.views.generic import CreateView
 from django.core.urlresolvers import reverse_lazy
 from .models import *
+from django.views.generic import ListView
 # Create your views here.
 class Home(TemplateView):
     template_name = "home.html"
@@ -10,8 +11,11 @@ class ReviewCreateView(CreateView):
   model = Review
   template_name = "review/review_form.html"
   fields = ['title', 'review description']
-  success_url = reverse_lazy('home')
+  success_url = reverse_lazy('review_list')
   
   def form_valid(self, form):
     form.instance.user = self.request.user
     return super (ReviewCreateView, self).form_valid(form)
+class ReviewListView(ListView):
+    model = Review
+    template_name = "review/review_list.html"
