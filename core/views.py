f72151ecb9699fcda76337d0312c5a6bca970b35
Matt <mjbowden@quinnipiac.edu>
2015-11-24 20:28:15 +0000

    Add reply create view

--- core/views.py
+++ core/views.py
@@ -13,7 +13,7 @@ class Home(TemplateView):
 class ReviewCreateView(CreateView):
   model = Review
   template_name = "review/review_form.html"
-  fields = ['title', 'review description']
+  fields = ['title', 'description']
   success_url = reverse_lazy('review_list')
 
   def form_valid(self, form):
@@ -33,3 +33,15 @@ class ReviewDeleteView(DeleteView):
     model = Review
     template_name = 'review/review_confirm_delete.html'
     sucess_url = reverse_lazy('review_list')
+class ReplyCreateView(CreateView):
+    model = Reply
+    template_name = "reply/reply_form.html"
+    fields = ['text']
+    
+    def get_success_url(self):
+      return self.object.review.get_absolute_url()
+
+    def form_valid(self, form):
+      form.instance.user = self.request.user
+      form.instance.review = Review.objects.get(id=self.kwargs['pk'])
+      return super (ReplyCreateView, self).form_valid(form)
